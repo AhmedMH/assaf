9d8f5e86e61b239697920c5e89f16f0e765e01bf
AhmedMH <enghamed126@gmail.com>
2015-06-29 11:35:32 +0200

    link added to logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.html">Assaf</a>
+          <a class="navbar-brand" href="#">Assaf</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
